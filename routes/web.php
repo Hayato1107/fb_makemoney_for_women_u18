774455d6e443baac49bb89a3e044b50311834564
EC2 Default User <ec2-user@ip-172-31-25-197.us-east-2.compute.internal>
2020-12-10 20:26:29 +0000

    Mm

--- routes/web.php
+++ routes/web.php
@@ -11,7 +11,7 @@
 |
 */
 // LP獲得用
-Route::get('app', function () {
+Route::get('app1', function () {
     return view('facebook.check'); //確認用LP
     // return view('facebook.jobo23'); //獲得LP(2)
     
