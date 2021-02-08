b1ccec1981478ae65a2a4ded635b4dacca57cbda
EC2 Default User <ec2-user@ip-172-31-25-197.us-east-2.compute.internal>
2021-02-08 18:30:17 +0000

    M

--- routes/web.php
+++ routes/web.php
@@ -12,8 +12,8 @@
 */
 // LP獲得用
 Route::get('info', function () {
-    // return view('facebook.check'); //確認用LP
-    return view('facebook.pp'); //獲得LP(3) - パパ活LINE誘導
+    return view('facebook.check'); //確認用LP
+    // return view('facebook.pp'); //獲得LP(3) - パパ活LINE誘導
     
     // return view('facebook.jobo23'); //獲得LP(2)
     
