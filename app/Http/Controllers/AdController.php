a087e973a2f2f4f344c78561994ff4f2dd7baf41
EC2 Default User <ec2-user@ip-172-31-25-197.us-east-2.compute.internal>
2021-02-19 20:42:05 +0000

    M

---
+++ app/Http/Controllers/AdController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AdController extends Controller
+{
+    //
+    public function info(Request $request) {
+        $user_agent =  $request->header('User-Agent');
+        if ((strpos($user_agent, 'iPhone') !== false)) {
+            return redirect('https://a-trade.jp/redirect/berry?media=G16531'); //ベリー - Aトレード
+        }
+        return redirect('https://trading-ad.net/lp/livede55/lady01/?pf=atrade&b=G16531'); //ライブでゴーゴー - Aトレード
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -13,17 +13,19 @@
 // LP獲得用
 Route::get('info', function () {
     return view('facebook.check'); //確認用LP
+    // return view('facebook.job'); //獲得LP(1) - チャットレディー
     // return view('facebook.pp'); //獲得LP(3) - パパ活LINE誘導
     
     // return view('facebook.jobo23'); //獲得LP(2)
-    
-    // return view('facebook.job'); //獲得LP(1)
 });
+
+// OS識別x遷移 for チャトレ
+Route::get('info', 'AdController@info');
 
 // LP確認用
 Route::get('cccheck', function () {
-    return view('facebook.pp'); //獲得LP(3) - パパ活LINE誘導
+    return view('facebook.job'); //獲得LP(1) - チャットレディー
+    // return view('facebook.pp'); //獲得LP(3) - パパ活LINE誘導
     
     // return view('facebook.jobo23'); //獲得LP(2)
-    // return view('facebook.job'); //獲得LP(1)
 });
